fd12e46a7f881721095ffc1080183d5d1f358d09
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2023-11-16 16:18:59 +0000

    Deploying to gh-pages from @ NatalaBoiko/mysite4students@10bb57abca094480f7f03181eb5081b54e785f8d 🚀

--- index.af16b0f7.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".about__moreBtn"),s=document.querySelector(".about p");console.log(s.className.includes("hideText"));e.addEventListener("click",(()=>{s.className.includes("hideText")?(s.classList.remove("hideText"),s.classList.add("showText")):(s.classList.add("hideText"),s.classList.remove("showText"))})),console.log("button.js");
-//# sourceMappingURL=index.af16b0f7.js.map
