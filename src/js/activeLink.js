b670227bd0db88847c19d5f1b492d35c070126af
Nataliia <natalabojko@gmail.com>
2024-02-01 16:32:06 +0200

    add active links

--- src/js/activeLink.js
+++ src/js/activeLink.js
@@ -1,21 +1,24 @@
 const sections = document.querySelectorAll("section");
-console.log(sections);
+// console.log(sections);
+
 const navLinks = document.querySelectorAll(".navLink");
-console.log(navLinks);
+// console.log(navLinks[0].href);
+
+console.log(window.location);
 
 window.onscroll = () => {
   sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 300;
+    let top = window.scrollY + 400;
+    let offset = sec.offsetTop;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
     if (top >= offset && top < offset + height) {
       navLinks.forEach((link) => {
         link.classList.remove("active");
 
-        document
-          .querySelector(".navLink[href*=" + id + "]")
-          .classList.add("active");
+        if (link.href.includes(id)) {
+          link.classList.add("active");
+        }
       });
     }
   });
